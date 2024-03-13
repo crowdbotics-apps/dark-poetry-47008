9d4d68cae7d638867fc5966cf56a0bf11ec1ad46
Crowdbotics <team@crowdbotics.com>
2024-03-13 18:09:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-poetry-47008",
+  url: "https://dark-poetry-47008.botics.co",
+  api: "https://dark-poetry-47008.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
